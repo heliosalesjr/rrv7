2b811db286b6682682150b9405e9d6d4adc62c32
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2025-05-13 15:09:45 -0300

    displaying the data on the front

--- app/routes/items.tsx
+++ app/routes/items.tsx
@@ -1,8 +1,35 @@
+import { supabase } from "~/supabase-client";
+import type { Route } from "../+types/root";
+import { Link } from "react-router";
 
-const items = () => {
-  return (
-    <div>items</div>
-  )
+
+export async function loader() {
+  const{data, error} = await supabase.from("items").select("*");
+  if (error) {
+    return { error: error.message };
+  }
+
+  return { items: data }
 }
 
-export default items
+export default function Items( { loaderData }: Route.ComponentProps) {
+  const { error, items } = loaderData;
+  return (
+  <div>
+    <h2 className="text-2xl py-2 font-semibold text-stone-500">List of items</h2>
+    {error && <p className="text-red-500">{error}</p>}
+    <ul className="space-y-4">
+      {items?.map((item) => (
+        <li className="p-4 bg-white shadow-md rounded-lg">
+          <Link to={"/"} className="block text-stone-600">
+            <span className="font-bold text-xl"> {item.title}</span>
+            <p className="text-stone-700"> {item.description}</p>
+          </Link>
+        </li>
+        
+      ))}
+    </ul>
+  </div>
+ )
+  
+}
